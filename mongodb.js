8281e412886fe9916d31b80739a59b1b7c12d8df
thomasdroege <ThomasDroege@gmx.de>
2020-05-19 21:14:35 +0200

    Read Tasks and Users out of mongo db

--- mongodb.js
+++ mongodb.js
@@ -5,11 +5,6 @@ const { MongoClient, ObjectID} = require('mongodb')
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-const id = new ObjectID()
-console.log(id.id.length)
-console.log(id.toHexString().length)
-
-
 // Deprecated constructor to MongoClient
 // useNewUrlParser: true 
 MongoClient.connect(connectionURL, { useUnifiedTopology: true}, (error, client) => {
@@ -18,51 +13,34 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true}, (error, client) 
     } 
 
     const db = client.db(databaseName)
+
+    db.collection('users').findOne({ _id: new ObjectID("5ec428679c6dd436e402427f")}, (error, user) =>{
+        if(error){
+            return console.log('Unable to fetch user')
+        }
+        
+        console.log(user)
+    })
     
-    // db.collection('users').insertOne({
-    //     _id: id,
-    //     name: 'Tom Hankblub',
-    //     age: 32
-    // }, (error, result) => {
-    //     if(error){
-    //         return console.log('Unable to insert user')
-    //     }
+    //gives back a pointer/cursor and save it to array 
+    db.collection('users').find({age: 32}).toArray( (error, users) => {
+       console.log(users) 
+    })
 
-    //     console.log(result.ops)
+    //gives back a pointer/cursor and save it to a number (less consumption of ram i guess)
+    db.collection('users').find({age: 32}).count( (error, count) => {
+        console.log(count) 
+     })
 
-    // })
+     db.collection('task').findOne({_id: new ObjectID("5ec2ed861dfc3a231cedba0a")}, (error, task) =>{
+         if(error){
+             return console.log('Unable to fetch task')
+         }
 
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'Leonie',
-    //         age: 31
-    //     }, {
-    //         name: 'Tom',
-    //         age: 33
-    //     }
-    // ], (error, result) => {
-    //     if(error){
-    //         return console.log('Unable to insert documents!')
-    //     }
-    //         console .log(result.ops)
-    // })
-
-    // db.collection('task').insertMany([
-    //     {
-    //         description: 'First task',
-    //         completed: true
-    //     }, {
-    //         description: 'Homeworks',
-    //         completed: false
-    //     }, {
-    //         description: 'cooking',
-    //         completed: false
-    //     }
-    // ], (error, result) => {
-    //     if(error){
-    //         return console.log('Unable to insert documents!')
-    //     } else{
-    //         console.log(result.ops)
-    //     }
-    // })
+         console.log(task)
+     })
+   
+     db.collection('task').find({completed: false}).toArray( (error, tasks) => {
+         console.log(tasks)
+     })
 })
