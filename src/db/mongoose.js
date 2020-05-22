465ec5feaf30a4ed5c0976e8560c220e567e3746
ThomasDroege <ThomasDroege@gmx.de>
2020-05-22 19:47:17 +0200

    added post REST request for tasks

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,14 +4,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
