69c9271b775d1e41b923b76d562240e4376b2c94
ThomasDroege <ThomasDroege@gmx.de>
2020-05-22 17:12:04 +0200

    implemented validator for valid email usage

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -7,24 +8,43 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email is invalid')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if(value < 0){
+                throw new Error('Age must be a positive number')
+            }
+        }
     }
 
 })
 
-// const me = new User({
-//     name: 'Thomas',
-//     age: 32
-// })
+const me = new User({
+    name: '   Thomas  ',
+    email: 'MYEMAIL@me.de    '
+})
 
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error!', error)
-// })
+me.save().then(() => {
+    console.log(me)
+}).catch((error) => {
+    console.log('Error!', error)
+})
 
 const Task = mongoose.model('Task', {
     description: {
@@ -35,13 +55,13 @@ const Task = mongoose.model('Task', {
     }
 })
 
-const newTask = new Task({
-    description: 'Cleaning the house',
-    stage: false
-})
+// const newTask = new Task({
+//     description: 'Cleaning the house',
+//     stage: false
+// })
 
-newTask.save().then(() => {
-    console.log(newTask)
-}).catch((error) =>{
-    console.log('Error!', error)
-})
+// newTask.save().then(() => {
+//     console.log(newTask)
+// }).catch((error) =>{
+//     console.log('Error!', error)
+// })
