db140645ae7ba8932dbf3e3ecedefed77b1272d3
ThomasDroege <ThomasDroege@gmx.de>
2020-05-24 11:17:23 +0200

    Hash user password

--- src/routers/user.js
+++ src/routers/user.js
@@ -48,7 +48,15 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try{
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+        // bypasses mongoose
+        //const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+
+
+        //needs to be done to not bypass mongoose and to hash the user password after updating user
+        const user =  await User.findById(req.params.id)
+        updates.forEach((update) => user[update] = req.body[update])
+
+        await user.save()
         
         if(!user){
             return res.status(404).send()
