cbfcc42980612ddcd18563880654e1e294dc7c59
ThomasDroege <ThomasDroege@gmx.de>
2020-05-25 23:01:22 +0200

    hidden password and token (2)

--- src/index.js
+++ src/index.js
@@ -17,16 +17,3 @@ app.use([userRouter, taskRouter])
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const jwt = require('jsonwebtoken')
-
-const myFunction = async () => {
-    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', {expiresIn: '7 days'})
-    console.log(token)
-
-    //same secret phrase need to be the second argument to verify the token
-    const data = jwt.verify(token, 'thisismynewcourse')
-    console.log(data)
-}
-
-myFunction()
