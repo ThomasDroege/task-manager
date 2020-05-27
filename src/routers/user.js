f9f353dd57ed0c36494725d1cf9b5602f46296e7
ThomasDroege <ThomasDroege@gmx.de>
2020-05-27 20:49:01 +0200

    Authenticating user endpoints (unauthorize user to manipulate others)

--- src/routers/user.js
+++ src/routers/user.js
@@ -53,23 +53,25 @@ router.get('/users/me', auth, async (req, res) => {
    res.send(req.user)
 })
 
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
+// route is not necessary for users, they shouldnt be allowed to get users by id!
 
-    try {
-        const user = await User.findById(_id)
+// router.get('/users/:id', async (req, res) => {
+//     const _id = req.params.id
 
-        if(!user) {
-            return res.status(404).send()
-        }
+//     try {
+//         const user = await User.findById(_id)
 
-        res.send(user)
-    } catch(e) {
-        res.status(500).send()
-    }
-})
+//         if(!user) {
+//             return res.status(404).send()
+//         }
 
-router.patch('/users/:id', async (req, res) => {
+//         res.send(user)
+//     } catch(e) {
+//         res.status(500).send()
+//     }
+// })
+
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -82,32 +84,32 @@ router.patch('/users/:id', async (req, res) => {
         // bypasses mongoose
         //const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
 
+        //obsolete since using auth method
+            //needs to be done to not bypass mongoose and to hash the user password after updating user
+            //const user =  await User.findById(req.params.id)
 
-        //needs to be done to not bypass mongoose and to hash the user password after updating user
-        const user =  await User.findById(req.params.id)
-        updates.forEach((update) => user[update] = req.body[update])
+        updates.forEach((update) => req.user[update] = req.body[update])
 
-        await user.save()
-        
-        if(!user){
-            return res.status(404).send()
-        }
-
-        res.send(user)
+        await req.user.save()
+        res.send(req.user)
     } catch(e) {
         res.status(400).send(e)
     }
 })
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id)
+        // the preferences of the authorized user (req.user.***) are avaible since the auth method is called
+        
+        // the next lines are not necessary anymore since the user is passed by the auth method
+            // const user = await User.findByIdAndDelete(req.user._id)
 
-        if(!user) {
-            return res.status(404).send()
-        }
+            // if(!user) {
+            //     return res.status(404).send()
+            // }
 
-        res.send(user)
+        await req.user.remove()
+        res.send(req.user)
     }catch(e) {
         res.status(500).send(e)
     }
